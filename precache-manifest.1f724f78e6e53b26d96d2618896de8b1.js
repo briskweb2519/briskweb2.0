9c648903b6aca94399000fcab2986f0608b13743
briskweb2519 <briskweb2519@gmail.com>
2020-10-05 21:42:32 +0530

    Updates

---
+++ precache-manifest.1f724f78e6e53b26d96d2618896de8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d949f37cc0b17e50c45d7737e0fafd23",
+    "url": "/briskweb2.0/index.html"
+  },
+  {
+    "revision": "bbccc6cbcf86115cbf5c",
+    "url": "/briskweb2.0/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "382a2f2ea3896f7cac6f",
+    "url": "/briskweb2.0/static/css/main.1d1cff36.chunk.css"
+  },
+  {
+    "revision": "bbccc6cbcf86115cbf5c",
+    "url": "/briskweb2.0/static/js/2.61f990a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/briskweb2.0/static/js/2.61f990a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382a2f2ea3896f7cac6f",
+    "url": "/briskweb2.0/static/js/main.a2926b6a.chunk.js"
+  },
+  {
+    "revision": "01fb0c5a33a08a2d9e50",
+    "url": "/briskweb2.0/static/js/runtime-main.f5449b44.js"
+  }
+]);

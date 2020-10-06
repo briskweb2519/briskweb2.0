d1bd02aef0f375f6a6eee325a45ce330c4a556a4
briskweb2519 <briskweb2519@gmail.com>
2020-10-06 15:12:49 +0530

    'commit'

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -11,10 +11,11 @@ class HeaderComponent extends Component {
     render() {
         return (
             <>
-                <Navbar sticky="top" collapseOnSelect expand="lg" className="p-3" style={{backgroundColor:"white"}}>
+
+                <Navbar sticky="top" collapseOnSelect expand="lg" className="p-3 mb-3" style={{ backgroundColor: "#195e83" }}>
                     <Container>
 
-                        <h3 style={{ color: "#195e83" }}>BRISKWEB</h3>
+                        <h3 style={{ color: "white" }}>BRISKWEB</h3>
                         <Navbar.Toggle class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="icon-bar top-bar"></span>
                             <span class="icon-bar middle-bar"></span>
@@ -22,12 +23,12 @@ class HeaderComponent extends Component {
                         </Navbar.Toggle>
 
                         <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="ml-auto text-center">
-                                <Link className="nav-link" to="/" style={{ color: "#195e83" }}>Home</Link>
-                                <Link className="nav-link" to="/services" style={{ color: "#195e83" }}>Services</Link>
-                                <Link className="nav-link" to="/project" style={{ color: "#195e83" }}>Projects</Link>
-                                <Link className="nav-link" to="/about" style={{ color: "#195e83" }}>About</Link>
-                                <Link className="nav-link" to="/contact" style={{ color: "#195e83" }}>Contact</Link >
+                            <Nav className="ml-auto text-centercontainer pullUp ">
+                                <Link className="nav-link" to="/" style={{ color: "white" }}>Home</Link>
+                                <Link className="nav-link" to="/services" style={{ color: "white" }}>Services</Link>
+                                <Link className="nav-link" to="/project" style={{ color: "white" }}>Projects</Link>
+                                <Link className="nav-link" to="/about" style={{ color: "white" }}>About</Link>
+                                <Link className="nav-link" to="/contact" style={{ color: "white" }}>Contact</Link >
                             </Nav>
                         </Navbar.Collapse>
                     </Container>

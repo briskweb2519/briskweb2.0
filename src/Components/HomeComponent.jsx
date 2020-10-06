454dcb5f93576a84e3748837fd762fb1ed5be410
briskweb2519 <briskweb2519@gmail.com>
2020-10-06 10:25:34 +0530

    'commit'

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -74,9 +74,9 @@ class HomeComponent extends Component {
                 </h4>
                     </div>
                     <div className="text-center mb-5 mt-5">
-                        <Image src="/Images/tab.png" width="12%" className="animate__animated animate__backInLeft tab" />
-                        <Image src="/Images/pc.png" width="50%" className="animate__animated animate__backInDown laptop" />
-                        <Image src="/Images/phone.png" width="5%" className="animate__animated animate__backInRight phone" />
+                        <Image src="/public/Images/tab.png" width="12%" className="animate__animated animate__backInLeft tab" />
+                        <Image src="/public/Images/pc.png" width="50%" className="animate__animated animate__backInDown laptop" />
+                        <Image src="/public/Images/phone.png" width="5%" className="animate__animated animate__backInRight phone" />
                     </div>
 
                     <CardColumns style={{ marginTop: "10%" }}>
